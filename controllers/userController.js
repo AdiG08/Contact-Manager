5905a1ef46a3bc1ee9e4e3e2f07ef5831f5698d9
Aditya Garg <gargaditya008@gmail.com>
2024-05-06 21:27:48 +0530

    Added JWT Access Token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,35 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 //@desc Register a User
 //@route POST /api/users/register
 //@access public
-const userRegister = asyncHandler(async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+
+    //Hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ", hashedPassword);
+    const user = await User.create({ username, email, password: hashedPassword });
+
+    console.log(`User created ${user}`);
+    if (user) {
+        res.status(201).json({ _id: user.id, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({ message: "Register the user" });
 });
 
@@ -11,7 +37,28 @@ const userRegister = asyncHandler(async (req, res) => {
 //@route POST /api/users/register
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    if (User && await (bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            }
+        },
+        process.env.ACCESS_TOKEN_SECERT,
+        { expiresIn:"5m" }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
 });
 
 //@desc Curretn User Info
@@ -21,4 +68,4 @@ const currentUser = asyncHandler(async (req, res) => {
     res.json({ message: "Current user information" });
 });
 
-module.exports = { userRegister ,loginUser, currentUser};
+module.exports = { registerUser ,loginUser, currentUser};
